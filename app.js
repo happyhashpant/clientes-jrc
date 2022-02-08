794d5716a881819fb9308c36a2a26146ad846bcb
Happy <ihangcf64@gmail.com>
2022-02-07 20:21:15 -0600

    Cookies

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var session = require('express-session');
+var session = require("express-session");
 var todoController = require("./controllers/todoController");
 
 var app = express();
@@ -8,7 +8,16 @@ app.set("view engine", "ejs");
 
 app.use(express.static("./"));
 app.use(express.json());
-app.use(session({secret: "Your secret key"}));
+app.use(cookieParser());
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(
+  sessions({
+    secret: "thisismysecrctekey",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
 
 todoController(app);
 
