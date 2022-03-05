6925e60b374a071f4b00553e320c0640e8fb1f77
Happy <ihangcf64@gmail.com>
2022-03-05 17:45:58 -0600

    S3 Changes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -23,8 +23,8 @@ const multerS3 = require("multer-s3");
 const uuid = require("uuid").v4;
 const aws = require("aws-sdk");
 aws.config.update({
-  accessKeyId: "AKIAZ5BJL3XRTVQYHUTE",
-  secretAccessKey: "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",
+  accessKeyId: "AKIAZ5BJL3XR25ZYNTCH",
+  secretAccessKey: "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",
 });
 const s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
